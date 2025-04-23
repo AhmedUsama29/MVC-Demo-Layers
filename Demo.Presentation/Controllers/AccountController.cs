294cb537723ade675cf38fdc6feaac0c31846e40
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-04-24 01:17:04 +0200

    Register View

---
+++ Demo.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Demo.Presentation.ViewModels.AccountViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Register(RegisterViewModel viewModel)
+        {
+            if (ModelState.IsValid) 
+            {
+
+            }
+            return View(viewModel);
+        }
+
+    }
+}
